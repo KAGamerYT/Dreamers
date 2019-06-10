badb766664a3b9fc9bd61c00790d87105fc92b7e
KAGamerYT <51527445+KAGamerYT@users.noreply.github.com>
2019-06-10 07:31:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['^اhelp','Type ^help','Dreamers'];
+    var setGame = ['^help','Type ^help','Dreamers'];
     var i = -1;
     var j = 0;
     setInterval(function (){
